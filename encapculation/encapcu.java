1cbd128be2a665f6a678c6f410866d1394cf2e37
shreyansh <shreyanshshivhari9@gmail.com>
2025-08-27 14:00:56 +0530

    version-1

---
+++ encapculation/encapcu.java
@@ -0,0 +1,27 @@
+
+package p1.encapculation;
+
+import java.lang.reflect.Field;
+import java.sql.SQLException;
+
+class empolyee {
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    double salary;
+    String name;
+}
+public class encapcu {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello");
+        empolyee emp = new empolyee();
+        Field fsal=empolyee.class.getDeclaredField("salary");
+        fsal.setAccessible(true);
+        System.out.println(fsal.get(emp));
+    }
+}
